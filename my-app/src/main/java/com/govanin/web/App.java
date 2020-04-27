904dbf3a83b7863c748564f45bcd17638a9ee186
Cloud User <ec2-user@ip-172-31-41-196.ap-south-1.compute.internal>
2020-04-27 02:01:45 +0000

    added pom

---
+++ my-app/src/main/java/com/govanin/web/App.java
@@ -0,0 +1,13 @@
+package com.govanin.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
